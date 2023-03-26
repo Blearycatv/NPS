c05667205e5dc0db4c0d16e0d92af3091487131d
Blearycatv <liu752093278@outlook.com>
2023-03-26 21:15:07 +0800

    1

--- send_data.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-# -*- coding:utf-8 -*-
-# @FileName  :send_data.py
-# @Time      :2023/3/23 19:49
-# @Author    :Blearycatv
-# 测试用程序
-
-if __name__ == "__main__":
-    run_code = 0
